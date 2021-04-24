e23e151fccbbd92688d77cc301b7249080ec79ce
simfony <49770335+wizard1238@users.noreply.github.com>
2021-04-23 22:38:51 -0700

    collection view implemented for all decks page

---
+++ jadeocr/Controllers/Home/DeckCell.swift
@@ -0,0 +1,14 @@
+//
+//  DeckCell.swift
+//  jadeocr
+//
+//  Created by Jeremy Tow on 4/22/21.
+//
+
+import UIKit
+
+class DeckCell: UICollectionViewCell {
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+}
--- jadeocr/Controllers/Home/DecksViewController.swift
+++ jadeocr/Controllers/Home/DecksViewController.swift
@@ -7,40 +7,100 @@
 
 import UIKit
 
-class DecksViewController: UIViewController, HomeDelegate {
-
-    @IBOutlet weak var pageControl: UISegmentedControl!
-    var pageViewController: DeckPageViewController!
+class DecksViewController: UIViewController {
     
-    var currDeckId: String?
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var decks: NSArray?
+    var deckId: String = ""
+    
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        self.collectionView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshTableView), for: .valueChanged)
+        
+        updateDecks()
     }
-
+    
+    @objc func refreshTableView() {
+        updateDecks()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("hi")
+        super.viewDidLayoutSubviews()
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    func updateDecks() {
+        DeckRequests.getAllDecks(completion: {result in
+            DispatchQueue.main.async(execute: {
+                if result.count != 0 {
+                    if let passed = result[0] as? Bool {
+                        if !passed {
+                            self.decks = []
+                        }
+                    } else {
+                        self.decks = result
+                    }
+                    self.collectionView.reloadData()
+                }
+                self.refreshControl.endRefreshing()
+            })
+        })
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? DeckPageViewController {
-            pageViewController = vc
-            pageViewController.homeDelegate = self
-        } else if let vc = segue.destination as? DeckInfoViewController {
-            vc.deckId = currDeckId
+        if let vc = segue.destination as? DeckInfoViewController {
+            vc.deckId = deckId
         }
     }
+}
 
-    @IBAction func pageSwitched(_ sender: Any) {
-        pageViewController.switchPage(index: pageControl.selectedSegmentIndex)
-        print(pageControl.selectedSegmentIndex)
+extension DecksViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let deck = decks?[indexPath[1]] as? Dictionary<String, Any> {
+            deckId = deck["deckId"] as? String ?? ""
+            self.performSegue(withIdentifier: "deckInfoSegue", sender: self)
+        }
+    }
+}
+
+extension DecksViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return decks?.count ?? 0
     }
     
-    func transition(deckId: String) {
-        currDeckId = deckId
-        self.performSegue(withIdentifier: "deckInfoSegue", sender: self)
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-    func switchIndicator(i: Int) {
-        pageControl.selectedSegmentIndex = i
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! DeckCell
+        
+        if decks?.count != 0 && decks?.count != nil {
+            if let deck = decks?[indexPath[1]] as? Dictionary<String, Any> {
+                cell.titleLabel?.text = deck["deckName"] as? String
+                cell.descLabel?.text = deck["deckDescription"] as? String
+            }
+        }
+        
+        cell.view.clipsToBounds = true
+        cell.view.layer.cornerRadius = 10
+        
+        return cell
+    }
+}
+
+extension DecksViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        print("layouted")
+        return CGSize(width: (collectionView.frame.size.width - 30) / 2, height: 100)
     }
 }
